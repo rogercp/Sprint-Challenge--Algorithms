6196e8febbca619168da72bb8fe59151ac268565
Roger Caballero <rogercp1234@yahoo.com>
2019-09-13 09:36:53 -0700

    test passing for count recursion

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -4,7 +4,12 @@ Your function should return a count of how many occurences of ***"th"*** occur w
 Your function must utilize recursion. It cannot contain any loops.
 '''
 def count_th(word):
-    
-    # TBC
-    
-    pass
+	count = 0
+	substring = "th"
+	my_str = word.find(substring)
+	if my_str >= 0:
+		my_word = word[my_str + len(substring):]
+		count = count + 1 + count_th(my_word)
+	return count
+
+print(count_th("thethetoothhurts"))
